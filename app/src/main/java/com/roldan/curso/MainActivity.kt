0694707d8279261a42895f978b7af33eebca40b6
PACKARD <serchr110389@gmail.com>
2024-01-26 00:18:46 -0600

    creando mi vista de tinder v1

--- app/src/main/java/com/roldan/curso/MainActivity.kt
+++ app/src/main/java/com/roldan/curso/MainActivity.kt
@@ -1,48 +1,84 @@
 package com.roldan.curso
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.roldan.curso.ui.theme.CursoTheme
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CursoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(
+                            brush = Brush.verticalGradient(
+                                listOf(
+                                    Color.Magenta,
+                                    Color.Cyan
+                                )
+                            )
+                        ),
+                    contentAlignment = Alignment.Center
                 ) {
-                    Greeting("Roldan")
-
-
+                    Column {
+                        Text(
+                            text = "It's a match!",
+                            color = Color.White,
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 45.sp
+                        )
+                        Text(
+                            text = "You and Julissa have liked each other",
+                            color = Color.White,
+                            fontSize = 16.sp
+                        )
+                    }
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .align(Alignment.BottomCenter)
+                            .padding(20.dp)
+                    ) {
+                        Button(
+                            colors = ButtonDefaults.buttonColors(containerColor = Color.White),
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(60.dp)
+                                .padding(bottom = 6.dp),
+                            onClick = {}
+                        ) {
+                            Text(text = "Message her", color = Color.Black)
+                        }
+                        Button(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(60.dp)
+                                .padding(top = 6.dp),
+                            onClick = {}
+                        ) {
+                            Text(text = "Keep swiping")
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CursoTheme {
-        Greeting("Android")
     }
 }
